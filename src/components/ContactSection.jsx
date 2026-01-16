531a2fc24d13f5dc694fbd6d86bfe5ca5d87c8c5
pprince-dhiman <dhimanprince262@gmail.com>
2026-01-17 01:13:57 +0530

    first commit

---
+++ src/components/ContactSection.jsx
@@ -0,0 +1,206 @@
+import {Github, Instagram, Linkedin, Mail, MapPin, Phone, Send, Twitch, Twitter,} from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useToast } from "@/hooks/use-toast";
+import { useState, useRef, useEffect } from "react";
+import emailjs from '@emailjs/browser';
+
+export const ContactSection = () => {
+  const { toast } = useToast();
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  let [name, setName] = useState("");
+  let [email, setEmail] = useState("");
+  let [message, setMessage] = useState("");
+
+  const form = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    emailjs.sendForm(
+      import.meta.env.VITE_EMAILJS_SERVICE_ID, 
+      import.meta.env.VITE_EMAILJS_TEMPLATE_ID, 
+      form.current, 
+      { publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,})
+      .then(() => {
+        toast({
+          title: "Message sent!",
+          description: "Thank you for your message. I'll get back to you soon.",
+        });
+
+        setName("");
+        setEmail("");
+        setMessage("");
+        
+        setIsSubmitting(false);
+      })
+      .catch((error) => {
+        toast({
+          title: "ERROR!",
+          description: error.text,
+        });
+        setIsSubmitting(false);
+      });
+  };
+
+  return (
+    <section id="contact" className="py-24 px-4 relative bg-secondary/30">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+          Get In <span className="text-primary"> Touch</span>
+        </h2>
+
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          Have a project in mind or want to collaborate? Feel free to reach out.
+          I'm always open to discussing new opportunities.
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+          <div className="space-y-8">
+            <h3 className="text-2xl font-semibold mb-6">
+              {" "}
+              Contact Information
+            </h3>
+
+            <div className="space-y-6 justify-center">
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Mail className="h-6 w-6 text-primary" />{" "}
+                </div>
+                <div>
+                  <h4 className="font-medium"> Email</h4>
+                  <a
+                    href="mailto:kumarprince39243@gmail.com"
+                    className="text-muted-foreground hover:text-primary transition-colors"
+                  >
+                    kumarprince39243@gmail.com
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Phone className="h-6 w-6 text-primary" />{" "}
+                </div>
+                <div>
+                  <h4 className="font-medium"> Phone</h4>
+                  <a
+                    href="tel:+91925****136"
+                    className="text-muted-foreground hover:text-primary transition-colors"
+                  >
+                    +91 925** **136
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <MapPin className="h-6 w-6 text-primary" />{" "}
+                </div>
+                <div>
+                  <h4 className="font-medium"> Location</h4>
+                  <a className="text-muted-foreground hover:text-primary transition-colors">
+                    Roorkee, Uttarakhand, India
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="pt-8">
+              <h4 className="font-medium mb-4"> Connect With Me</h4>
+              <div className="flex space-x-4 justify-center">
+                <a href="https://www.linkedin.com/in/prince-kumar-980654291/" target="_blank">
+                  <Linkedin />
+                </a>
+                <a href="https://x.com/PrinceK9535221" target="_blank">
+                  <Twitter />
+                </a>
+                <a href="https://www.instagram.com/princek92776/" target="_blank">
+                  <Instagram />
+                </a>
+                <a href="https://github.com/pprince-dhiman/" target="_blank">
+                  <Github />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-card p-8 rounded-lg shadow-xs">
+            <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3>
+
+            <form ref={form} onSubmit={handleSubmit} className="space-y-6">
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
+                  Your Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  onChange={(e)=> setName(e.target.value)}
+                  value={name}
+                  required
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
+                  placeholder="Pedro Machado..."
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
+                  Your Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  onChange={(e)=> setEmail(e.target.value)}
+                  value={email}
+                  required
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
+                  placeholder="john@gmail.com"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
+                  Your Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  onChange={(e)=> setMessage(e.target.value)}
+                  value={message}
+                  required
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary resize-none"
+                  placeholder="Hello, I'd like to talk about..."
+                />
+              </div>
+
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className={cn(
+                  "cosmic-button w-full flex items-center justify-center gap-2"
+                )}
+              >
+                {isSubmitting ? "Sending..." : "Send Message"}
+                <Send size={16} />
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import { ArrowUp } from "lucide-react";
+
+export const Footer = () => {
+  return (
+    <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
+      {" "}
+      <p className="text-sm text-muted-foreground">
+        {" "}
+        <span className="mr-5">&copy; {new Date().getFullYear()}</span> <span className="mr-5">PrinceTech.co.</span> <span className="mr-5">All rights reserved.</span>
+      </p>
+      <a
+        href="#hero"
+        className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors"
+      >
+        <ArrowUp size={20} />
+      </a>
+    </footer>
+  );
+};
